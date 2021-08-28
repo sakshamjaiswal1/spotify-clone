c680a10deca5e010552710689a19afe10dabca03
Saksham Jaiswal <sjais973@gmail.com>
2021-08-28 16:36:27 +0530

    reducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+import { useEffect ,useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import Player from './Player';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify=new SpotifyWebApi()
 
 function App() {
+const [token, setToken] = useState(null)
+
+// Runs code on a given condition
+  useEffect(()=>{
+const hash=getTokenFromUrl()
+
+window.location.hash=''
+const _token=hash.access_token
+console.log('i have a token',_token)
+if(_token){
+  setToken(_token)
+  spotify.setAccessToken(_token)
+  spotify.getMe().then(user=>{
+    console.log('user',user)
+  })
+}
+
+  },[])
+
+
   return (
     <div className="app">
-  
-   {/* spotify Logo */}
-   {/* login */}
-   <Login/>
+  {
+    token ? (<Player/>):(<Login/>)
+  }
+
+   
     </div>
   );
 }
